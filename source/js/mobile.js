14e9d3ef78dbecee4b7d2f6a397e7c92f77625ee
HyanCat <hyancat@live.cn>
2016-04-12 15:07:10 +0800

    update

--- source/js/mobile.js
+++ source/js/mobile.js
@@ -131,7 +131,6 @@ define([], function(){
 
 	var resetTags = function(){
 		var tags = $(".tagcloud a");
-		tags.css({"font-size": "12px"});
 		for(var i=0,len=tags.length; i<len; i++){
 			var num = tags.eq(i).html().length % 5 +1;
 			tags[i].className = "";
